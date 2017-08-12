b1c4bcada1fb0fabf42f508f7868e6071fe594ec
Andrey Matveev <andrey93.28@gmail.com>
2017-08-12 13:25:37 +0300

    Create design of login page

--- app/src/main/java/com/ua/plamber_android/LoginActivity.java
+++ app/src/main/java/com/ua/plamber_android/LoginActivity.java
@@ -1,13 +1,50 @@
 package com.ua.plamber_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button mLoginButton;
+    private Button mSignButton;
+    private EditText mEmailLoginEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mLoginButton = (Button) findViewById(R.id.btn_login);
+        mSignButton = (Button) findViewById(R.id.btn_signup);
+        mEmailLoginEdit = (EditText) findViewById(R.id.et_email_login);
+
+        initBackgroundImage();
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mEmailLoginEdit.setError("Please enter email");
+            }
+        });
+
+        mSignButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initBackgroundImage() {
+        ImageView background = (ImageView) findViewById(R.id.iv_login_background);
+        Glide.with(this).load(R.drawable.main_background).centerCrop().into(background);
     }
 }
---
+++ app/src/main/java/com/ua/plamber_android/SignUpActivity.java
@@ -0,0 +1,24 @@
+package com.ua.plamber_android;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        initBackgroundImage(SignUpActivity.this);
+    }
+
+    private void initBackgroundImage(Context context) {
+        ImageView background = (ImageView) findViewById(R.id.iv_singup_background);
+        Glide.with(context).load(R.drawable.main_background).centerCrop().into(background);
+    }
+}
