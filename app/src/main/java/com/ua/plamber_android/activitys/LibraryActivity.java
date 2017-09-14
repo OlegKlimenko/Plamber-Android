83ef91789a1b5ab632529216712606546c929e0b
Andrey Matveev <andrey93.28@gmail.com>
2017-09-14 22:57:35 +0300

    Add navigation drawer

--- app/src/main/java/com/ua/plamber_android/activitys/LibraryActivity.java
+++ app/src/main/java/com/ua/plamber_android/activitys/LibraryActivity.java
@@ -2,11 +2,19 @@ package com.ua.plamber_android.activitys;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.ua.plamber_android.R;
 import com.ua.plamber_android.adapters.ViewPagerAdapter;
@@ -22,6 +30,10 @@ public class LibraryActivity extends AppCompatActivity {
     @BindView(R.id.toolbar) Toolbar mToolbar;
     @BindView(R.id.main_view_pager) ViewPager mViewPager;
     @BindView(R.id.tabLayout) TabLayout mTabLayout;
+    @BindView(R.id.navigation_view) NavigationView mNavigationView;
+    @BindView(R.id.drawerLayout) DrawerLayout mDrawerLayout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +44,13 @@ public class LibraryActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
 
         setupPager();
+
+        setupNavigationDrawer();
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
+                R.string.drawer_open, R.string.drawer_close);
+        mDrawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
     }
 
     public void setupPager() {
@@ -39,9 +58,23 @@ public class LibraryActivity extends AppCompatActivity {
         adapter.addFragment(new LibraryFragment(), "Library");
         adapter.addFragment(new UserBookFragment(), "My books");
         adapter.addFragment(new RecommendedFragmnet(), "Recommended");
-        
+
         mViewPager.setAdapter(adapter);
         mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    public void setupNavigationDrawer() {
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.nav_library:
+                       // mDrawerLayout.closeDrawers();
+                }
+                return true;
+            }
+        });
     }
 
     public static Intent startLibraryActivity(Context context) {
